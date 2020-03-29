4409e23e513f3ed81b42f7cfd4f6edff2b1fb2dd
Han Li <vcfvct@gmail.com>
2020-03-29 19:23:10 -0400

    symbol links

---
+++ sLinks.sh
@@ -0,0 +1,13 @@
+ln -s ~/GIT/dotfile/.zshrc ~/.zshrc
+ln -s ~/GIT/dotfile/.vimrc ~/.vimrc
+ln -s ~/GIT/dotfile/.coc.vim ~/.coc.vim
+ln -s ~/GIT/dotfile/.tmux.conf ~/.tmux.conf
+ln -s ~/GIT/dotfile/.tmux.conf.local ~/.tmux.conf.local
+ln -s ~/GIT/dotfile/.gitconfig ~/.gitconfig
+ln -s ~/GIT/dotfile/.gitignore ~/.gitignore
+ls -s ~/GIT/.vim/autoload/lightline/colorscheme/onedark.vim ~/.vim/autoload/lightline/colorscheme/onedark.vim
+ls -s ~/GIT/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+ls -s ~/GIT/.config/nvim/init.vim ~/.config/nvim/init.vim
+ls -s ~/GIT/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ls -s ~/GIT/.config/ripgrep/rc ~/.config/ripgrep/rc
+
