a2c97ad7e5b689f44b575259cdfc475e24925053
Han Li <vcfvct@gmail.com>
2020-03-31 18:21:03 -0400

    update symbol link file

--- sLinks.sh
+++ sLinks.sh
@@ -1,13 +1,36 @@
+rm ~/.zshrc
 ln -s ~/GIT/dotfile/.zshrc ~/.zshrc
+
+rm ~/.vimrc
 ln -s ~/GIT/dotfile/.vimrc ~/.vimrc
+
+rm ~/.coc.vim
 ln -s ~/GIT/dotfile/.coc.vim ~/.coc.vim
+
+rm ~/.tmux.conf
 ln -s ~/GIT/dotfile/.tmux.conf ~/.tmux.conf
+
+rm ~/.tmux.conf.local
 ln -s ~/GIT/dotfile/.tmux.conf.local ~/.tmux.conf.local
+
+rm ~/.gitconfig
 ln -s ~/GIT/dotfile/.gitconfig ~/.gitconfig
+
+rm ~/.gitignore
 ln -s ~/GIT/dotfile/.gitignore ~/.gitignore
-ln -s ~/GIT/.vim/autoload/lightline/colorscheme/onedark.vim ~/.vim/autoload/lightline/colorscheme/onedark.vim
-ln -s ~/GIT/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s ~/GIT/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/GIT/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-ln -s ~/GIT/.config/ripgrep/rc ~/.config/ripgrep/rc
+
+rm ~/.vim/autoload/lightline/colorscheme/onedark.vim
+ln -s ~/GIT/dotfile/.vim/autoload/lightline/colorscheme/onedark.vim ~/.vim/autoload/lightline/colorscheme/onedark.vim
+
+rm ~/.config/alacritty/alacritty.yml
+ln -s ~/GIT/dotfile/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+rm ~/.config/nvim/init.vim
+ln -s ~/GIT/dotfile/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+rm ~/.config/nvim/coc-settings.json
+ln -s ~/GIT/dotfile/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+rm ~/.config/ripgrep/rc
+ln -s ~/GIT/dotfile/.config/ripgrep/rc ~/.config/ripgrep/rc
 
