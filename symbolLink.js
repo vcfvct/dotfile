cc3ce8a51ecb9818f39d155108926201787952e5
Han Li <vcfvct@gmail.com>
2020-04-01 18:48:23 -0400

    update file list

--- symbolLink.js
+++ symbolLink.js
@@ -5,7 +5,16 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 (async () => {
-  const fileList = ['.zshrc', '.vimrc', '.coc.vim', '.tmux.conf', '.tmux.conf.local', '.gitconfig', '.gitignore', '.vim/autoload/lightline/colorscheme/onedark.vim'];
+  const fileList = [
+    '.zshrc',
+    '.vimrc',
+    '.coc.vim',
+    '.tmux.conf',
+    '.tmux.conf.local',
+    '.gitconfig',
+    '.gitignore',
+    '.vim/autoload/lightline/colorscheme/onedark.vim'
+  ];
 
   await Promise.all(fileList.map(f => createSymbolLink(f)));
 
@@ -14,9 +23,9 @@ const exec = util.promisify(require('child_process').exec);
     const dest = `${process.env.HOME}/${filePath}`;
     let cmd = `rm ${dest}`;
     console.log(`executing: ${cmd}`);
-    await exec(cmd);
+    // await exec(cmd);
     cmd = `ln -s ${src} ${dest}`;
     console.log(`executing: ${cmd}`);
-    await exec(cmd);
+    // await exec(cmd);
   }
 })();
