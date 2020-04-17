c180c2e940967aeb921771a3ed7e69e1295125a4
Han Li <vcfvct@gmail.com>
2020-04-17 18:46:35 -0400

    add new fzf find edit function

---
+++ .config/fish/functions/fzf_find_edit.fish
@@ -0,0 +1,12 @@
+function fzf_find_edit
+  set -l file (
+    fzf --no-multi --select-1 --exit-0 \
+        --preview 'bat --color=always --line-range :500 {}'
+    )
+  # double quote is required.
+  if test -n "$file" 
+    $EDITOR "$file"
+  end
+  commandline -f repaint
+end
+
