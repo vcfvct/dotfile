3c173ebb41d15e86ef3db899364229490ffbfeaa
Han Li <vcfvct@gmail.com>
2020-05-20 16:53:34 -0400

    fix hisotry search function name

--- .config/fish/functions/fzf_reverse_isearch.fish
+++ .config/fish/functions/fzf_reverse_isearch.fish
@@ -1,4 +1,4 @@
-function __fzf_reverse_isearch
+function fzf_reverse_isearch
     history merge
     history -z | eval fzf --read0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | perl -pe 'chomp if eof' | read -lz result
     and commandline -- $result
