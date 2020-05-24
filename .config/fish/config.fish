2744d55085febe9fdd97fc5eb11a3e02da1ae385
Han Li <vcfvct@gmail.com>
2020-05-23 20:56:30 -0400

    fix syntax

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,6 +41,6 @@ bind -M insert \ck history-search-backward
 
 # set clipboard support for WSL
 if uname -r | grep 'microsoft' > /dev/null 
-  set -l LOCAL_IP (cat /etc/resolv.conf | grep nameserver | awk '({print $2}')
+  set -l LOCAL_IP (cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
   set -xg DISPLAY $LOCAL_IP:0
 end
