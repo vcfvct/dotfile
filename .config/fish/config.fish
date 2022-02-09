68825ddb516048df5a7acdb8e890b3d6dd309a01
Han Li <han.li@capitalone.com>
2022-02-09 08:20:40 -0500

    merge master

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -58,7 +58,7 @@ set -gx PATH /usr/local/bin $PATH
 function __check_rvm --on-variable PWD --description 'Do nvm stuff'
   status --is-command-substitution; and return
   if test -f .nvmrc; and test -r .nvmrc;
-    nvm install 
+    nvm use
   else
   end
 end
