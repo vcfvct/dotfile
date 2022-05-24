e3e068300911641c976148a3511b066c002b11a6
Han Li <vcfvct@gmail.com>
2022-05-24 16:21:30 -0400

    use apt for fzf

--- wsl/install.sh
+++ wsl/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 sudo apt update -y
-sudo apt install zip
+sudo apt install zip -y
 
 ## fish 3.x
 sudo apt-add-repository ppa:fish-shell/release-3 -y
-sudo apt update
+sudo apt update -y
 sudo apt install fish -y
 
 ## neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
+sudo apt update -y
 sudo apt install neovim -y
 
 ## vimPlug
@@ -18,16 +18,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ## tmux
-sudo apt install tmux
+sudo apt install tmux -y
 
 ## fzf
 # sample: https://github.com/junegunn/fzf/releases/download/0.29.0/fzf-0.29.0-linux_amd64.tar.gz
-fzf_url=`curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep -wo "https.*linux_amd64.tar.gz"`
-fzf_tar=fzf.tar.gz
-curl -L $fzf_url --output $fzf_tar
-tar -xzf $fzf_tar
-sudo mv fzf /usr/bin/
-rm $fzf_tar
+#fzf_url=`curl -s https://api.github.com/repos/junegunn/fzf/releases/latest | grep -wo "https.*linux_amd64.tar.gz"`
+#fzf_tar=fzf.tar.gz
+#curl -L $fzf_url --output $fzf_tar
+#tar -xzf $fzf_tar
+#sudo mv fzf /usr/bin/
+#rm $fzf_tar
+sudo apt install fzf -y
 
 ## rg
 # sample: https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
