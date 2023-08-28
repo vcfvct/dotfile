ca0ee48b0429ba8609821b811950eb872f887add
Han Li <vcfvct@gmail.com>
2023-08-28 19:14:20 -0400

    add sap/omap file json type

--- .config/astro/plugins/user.lua
+++ .config/astro/plugins/user.lua
@@ -14,6 +14,7 @@ return {
   -- nomarl_delete = vim.keymap.set("n", "d", '"_d', default_map_opt),
   format_json = vim.api.nvim_command "com! FormatJSON %!python3 -m json.tool",
   format_xml = vim.api.nvim_command 'com! FormatXML :%!python3 -c "import xml.dom.minidom, sys; print(xml.dom.minidom.parse(sys.stdin).toprettyxml())"',
+  file_type = vim.api.nvim_command "au BufNewFile,BufRead *.sap,*.omap setf json",
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
