117dbb2bfce14a26f2b318a1088874255c449cd8
Han Li <vcfvct@gmail.com>
2023-10-08 11:13:29 -0400

    add js debug config

---
+++ .config/astro/plugins/dap.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      { "theHamsta/nvim-dap-virtual-text", config = true },
+    },
+    config = function()
+      local dap = require "dap"
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "js-debug-adapter", -- As I'm using mason, this will be in the path
+          args = { "${port}" },
+        }
+      }
+
+      for _, language in ipairs { "typescript", "javascript" } do
+        require("dap").configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
+  },
+}
