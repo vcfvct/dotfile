747af2df5b69d6cd3184cbc9006ad1dd7c7f58da
Han Li <vcfvct@gmail.com>
2024-01-19 16:47:38 -0500

    use PSReadLine ViModeIndicator option to toggle cursor

--- wsl/powershell_profile.ps1
+++ wsl/powershell_profile.ps1
@@ -5,7 +5,19 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Exp
 
 Set-PSReadLineOption -PredictionSource History
 
+# pwsh vi mode
 Set-PSReadlineOption -EditMode vi
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+
 # use `ctrl+[` to exit from Edit mode to Command mode
 Set-PSReadLineKeyHandler -Chord 'Ctrl+Oem4' -Function ViCommandMode
 
