e5fd802cbc438f833f50e2be3534770b52d43ed1
Han Li <vcfvct@gmail.com>
2024-04-14 21:32:29 +0000

    add f2 to lsp rename

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -1,4 +1,3 @@
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -84,7 +83,8 @@ return {
     mappings = {
       n = {
         gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
-        gi = { function() vim.lsp.buf.implementation() end, desc = "Implementation of current symbol",},
+        gi = { function() vim.lsp.buf.implementation() end, desc = "Implementation of current symbol" },
+        ["<F2>"] = { function() vim.lsp.buf.rename() end, desc = "Rename current symbol" },
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         -- gD = {
         --   function() vim.lsp.buf.declaration() end,
