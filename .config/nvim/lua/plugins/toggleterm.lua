a141189111173c4214f4a092a453c82de13251c9
Han Li <vcfvct@gmail.com>
2026-06-07 20:22:20 +0000

    use float for toggle term

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    direction = "float",
+    float_opts = {
+      border = "rounded",
+      width = 120,
+      height = 30,
+    },
+  },
+}
